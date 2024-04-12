2028bdb4abedfb3902e6f3b754aed9cdc4de7f3f
Larry Grullon-Polanco <larrygrpolanco@gmail.com>
2024-04-12 03:20:27 -0400

    Working!
    Time to read more research on language input

--- Dialogue_Dictionary.py
+++ Dialogue_Dictionary.py
@@ -11,7 +11,7 @@ st.markdown(
 
 col1, col2 = st.columns(2)
 with col1:
-    practice_language = st.text_input(
+    target_language = st.text_input(
         "What language are you learning?",
         placeholder="Chinese, Spanish, etc.",
     )
@@ -29,7 +29,7 @@ with col2:
         ],
     )
 
-if practice_language:  # if practice_language is not empty
+if target_language:  # if practice_language is not empty
 
     (
         tab1,
@@ -42,99 +42,74 @@ if practice_language:  # if practice_language is not empty
     )
 
     with tab1:  # Dictionary
-        vocab_text = st.text_input(
+        vocab = st.text_input(
             "Enter the word or words you want to see used in a conversation:",
             placeholder="Vocabulary",
             help="Additional options in the sidebar located at the top left",
         )
 
     with tab2:  # Extra settings
-        col1, col2 = st.columns(2)
-        with col1:
-            conversation_context = st.text_area(
-                "Context",
-                placeholder="Ordering food at a restaurant in outer space.",
-                help="Specify a context to focus the conversation, e.g., ordering at a restaurant, asking for directions.",
-            )
-            formality = st.select_slider(
-                "Formality",
-                [
-                    "Informal",
-                    "Balanced",
-                    "Formal",
-                ],
-                value="Balanced",
-            )
+        conversation_context = st.text_area(
+            "Context",
+            placeholder="Ordering food at a restaurant in outer space.",
+            help="Give a context to focus the conversation, e.g., ordering at a restaurant, asking for directions.",
+        )
 
-        with col2:
-            preferred_language = st.text_input(
-                "Preferred Language",
-                value="English",
-                help="Set language for the explanations and translations.",
-            )
-            translation_on = st.toggle(
-                f"{preferred_language} explanations",
-                help=f"Check this to request {preferred_language} translations and cultural explinations.",
-            )
-            highlight_mistakes_on = st.toggle(
-                "Show common mistakes",
-                help="Check this to show common mistakes learners might make.",
-            )
+        col1, col2 = st.columns(2)
+        give_translation = col1.toggle("English Translations")
+        formality = col2.select_slider(
+            "Formality",
+            [
+                "Informal",
+                "Semi-informal",
+                "Balanced",
+                "Semi-formal",
+                "Formal",
+            ],
+            value="Balanced",
+        )
 
     # Updated to reflect the current settings
     current_settings = {
         "conversation_context": conversation_context,
         "formality": formality,
-        "preferred_language": preferred_language,
-        "translation_on": translation_on,
-        "highlight_mistakes_on": highlight_mistakes_on,
-        "practice_language": practice_language,
+        "give_translation": give_translation,
+        "target_language": target_language,
         "learner_level": learner_level,
     }
 
     if "responses" not in st.session_state:
         st.session_state["responses"] = []
 
+    if "find_clicked" not in st.session_state:
+        st.session_state["find_clicked"] = False
+
     col1, col2, col3, col4 = st.columns(4)
+
+    if col1.button("Find Conversation"):
+        st.session_state["find_clicked"] = True
+
     if col4.button("Clear Conversations"):
         st.session_state["responses"] = []  # Reset the list of responses
+        st.session_state["find_clicked"] = False
 
-    # Generating conversation using the corrected instance
-    if vocab_text and practice_language and learner_level:
-        if not vocab_text.strip():
-            st.warning("Enter a vocabulary word.")
-
-            # Create an instance of the GeminiProcessor
-            gemini = GeminiProcessor
-
-            # Apply the settings to your processor
-            gemini.set_settings(current_settings)
+    if st.session_state["find_clicked"]:
+        gemini = GeminiProcessor()
+        gemini.set_settings(current_settings)
 
         with st.spinner("Creating your dialogue..."):
-            prompt = gemini.create_convo_prompt(vocab_text)
+            response = gemini.generate_convo(vocab)
 
-            # Run LLM model
-            if llm_choice == "Google Gemini-Pro":
-                gemini_processor = GeminiProcessor(
-                    st.secrets["GOOGLE_API_KEY"], st.secrets["OPENAI_API_KEY"]
-                )
-                response = gemini_processor.generate_convo(prompt)
-            elif llm_choice == "OpenAI ChatGPT 4":
-                chatgpt_processor = ChatGPTProcessor(
-                    st.secrets["GOOGLE_API_KEY"], st.secrets["OPENAI_API_KEY"]
-                )
-                response = chatgpt_processor.generate_convo(prompt)
-
-            # Append new response to the start of the list so it appears at the top
             st.session_state["responses"].insert(0, response)
 
-            # Limit the number of responses to a specific max value
             max_responses = 5
             if len(st.session_state["responses"]) > max_responses:
                 # Remove the oldest response(s) to maintain only a max number of responses
                 st.session_state["responses"] = st.session_state["responses"][
                     :max_responses
                 ]
+
+        st.session_state["find_clicked"] = False
 
     for response in st.session_state["responses"]:
         st.info(response)
--- genai_processor.py
+++ genai_processor.py
@@ -1,76 +1,59 @@
 import google.generativeai as genai
 import streamlit as st
 
-
+genai.configure(api_key=st.secrets["google_api_key"])
+model = genai.GenerativeModel("gemini-pro")
 
 class GeminiProcessor:
     def __init__(self):
-        self.google_api_key = st.secrets["google_api_key"]
         self.settings = {
             "conversation_context": "",
             "formality": "Balanced",
-            "preferred_language": "English",
-            "translation_on": False,
-            "highlight_mistakes_on": False,
-            "practice_language": "",
+            "give_translation": False,
+            "target_language": "",
             "learner_level": "",
         }
-
 
     def set_settings(self, settings_dict):
         for key, value in settings_dict.items():
             if key in self.settings:
                 self.settings[key] = value
 
-
-
-    def create_convo_prompt(self, vocab):
-        preferred_language = self.settings["preferred_language"]
-        translation_on = self.settings["translation_on"]
-        highlight_mistakes_on = self.settings["highlight_mistakes_on"]
-        practice_language = self.settings["practice_language"]
+    def create_prompt(self, vocab):
+        target_language = self.settings["target_language"]
         learner_level = self.settings["learner_level"]
+        give_translation = self.settings["give_translation"]
         conversation_context = self.settings["conversation_context"]
         formality = self.settings["formality"]
 
         translation_request = (
-            f"Proivde a translation into the learner's preferred language ({preferred_language}). Emphasize clarity and accuracy in your translation. Where relevant, include brief annotations or explanations to highlight cultural or contextual nuances. These insights should elucidate expressions, idioms, or cultural references that may not directly translate but are crucial for understanding the dialogue's deeper meanings and implications."
-            if translation_on
+            f"Proivde an english translation. Emphasize clarity and accuracy in your translation. Where relevant, include brief annotations or explanations to highlight cultural or contextual nuances. These insights should elucidate expressions, idioms, or cultural references that may not directly translate but are crucial for understanding the dialogue's deeper meanings and implications."
+            if give_translation
             else "Provide the dialogue with no translations. Focus on ensuring the dialogue is engaging and educational within the parameters set, allowing the learner to immerse fully in the practice language without direct translation. This approach encourages deeper language intuition and context-based understanding."
-        )
-        mistakes_request = (
-            f"Identify common errors learners might commit when using the vocabulary word '{vocab}'. Illuminate these mistakes by providing a brief explanation of why they are incorrect. Enhance this learning moment by crafting 2-3 model sentences that demonstrate the correct usage of '{vocab}'. These sentences should not only rectify the identified mistakes but also serve as clear examples for learners to emulate, helping them to internalize the correct application of the word in various contexts."
-            if highlight_mistakes_on
-            else ""
         )
 
         prompt = f"""
-        "Create a dialogue in {practice_language}, tailored specifically to the CEFR level {learner_level}. Your objective is to seamlessly incorporate the target vocabulary word '{vocab}' into a conversation that is relevant to the given theme or context, '{conversation_context}'. Please adhere to the following guidelines to ensure a high-quality learning experience:
+        "Create a dialogue in {target_language}, tailored specifically to the CEFR level {learner_level}. Your objective is to seamlessly incorporate the target vocabulary word '{vocab}' into a conversation that is relevant to the given theme or context, '{conversation_context}'. Please adhere to the following guidelines to ensure a high-quality learning experience:
 
-        Scenario Introduction: Begin with a concise description of the scenario in the learner's preferred language, '{preferred_language}'. This description should be engaging and clear, setting the stage for the dialogue. Briefly outline the setting, characters involved, and the situation they are in, making sure it aligns with the theme/context '{conversation_context}'.
+        Scenario Introduction: Begin with a concise description of the scenario in English. This description should be engaging and clear, setting the stage for the dialogue. Briefly outline the setting, characters involved, and the situation they are in, making sure it aligns with the theme/context '{conversation_context}'.
 
         Dialogue Construction:
 
-        Compose 3-5 exchanges between characters in {practice_language}, ensuring the dialogue is realistic and relevant to the learners' experiences.
+        Compose 3-5 exchanges between characters in {target_language}, ensuring the dialogue is realistic and relevant to the learners' experiences.
         Integrate the target vocabulary word '{vocab}' naturally into the conversation. Use the word in different forms or contexts if possible to show its versatility.
-        Adjust the dialogue to match the specified {practice_language} CEFR level '{learner_level}', considering sentence complexity, vocabulary, and grammatical structures appropriate for that level.
+        Adjust the dialogue to match the specified {target_language} CEFR level '{learner_level}', considering sentence complexity, vocabulary, and grammatical structures appropriate for that level.
         Formality Register: Ensure the dialogue reflects the requested level of formality ('{formality}'). This could range from informal, using colloquial language and contractions, to formal, employing polite forms, professional terminology, and complete sentences.
 
-        Dialogue Length and Complexity: Aim for a total word count of approximately 100-150 words for the entire dialogue. This ensures enough {practice_language} content for educational value without overwhelming the learner. Sentences should vary in length and complexity according to the CEFR level specified.
+        Dialogue Length and Complexity: Aim for a total word count of approximately 100-150 words for the entire dialogue. This ensures enough {target_language} content for educational value without overwhelming the learner. Sentences should vary in length and complexity according to the CEFR level specified.
 
-        Ensure that your {practice_language} dialogue is not only a learning tool but also a means for reflection and deeper engagement with the language. The goal is to make each dialogue a stepping stone towards fluency, providing learners with practical language skills they can apply in real-world situations.
-
-        {mistakes_request}
+        Ensure that your {target_language} dialogue is not only a learning tool but also a means for reflection and deeper engagement with the language. The goal is to make each dialogue a stepping stone towards fluency, providing learners with practical language skills they can apply in real-world situations.
 
         {translation_request}
         """
         return prompt
 
-
-
-    def generate_convo(self, prompt):
-        model = gemini.GenerativeModel("gemini-pro")
+    def generate_convo(self, vocab):
+        prompt = self.create_prompt(vocab)
         response = model.generate_content(prompt)
 
         return response.text
-
